07aefa9e979d154f60ee3cb2d13cd0dd33284354
mikaelaalu <54809602+mikaelaalu@users.noreply.github.com>
2019-10-23 15:45:46 +0200

    Order by date

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php
 declare(strict_types=1);
-// This is the file where you can keep all your functions. Remember to NOT
-// execute/run any functions in this file. Keep it dumb.
+require __DIR__. '/data.php'; 
+
+
+function compare($a, $b){
+ return strtotime($b['date']) - strtotime($a['date']);
+}
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 require __DIR__. '/functions.php';
 require __DIR__. '/data.php';
+usort ($posts, 'compare');
 
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
@@ -13,10 +14,33 @@ require __DIR__. '/data.php';
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <style></style>
-        <title>Plain News</title>
+        <link rel="stylesheet" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+        <title>Fake News</title>
     </head>
     <body>
+<header> 
+   <h1> Fake News </h1>
+</header>
+
+<div>
+
+    <?php foreach ($posts as $post) : ?>
+    <article>
+    <h2> <?php echo $post ['title']; ?> </h2>
+    <p> <?php echo $post ['content'] ?> </p>
+    
+    <ul> <li> <?php echo "Author " . $post['author'] ?> </li>
+    <li> <?php echo "Date " . $post ['date']  ?> </li>
+    <li> <?php echo "Likes " . $post ['likes'] ?> </li> </ul>
+    </article>
+    <?php endforeach;?>
+
+</div>
+
+
+
+
     </body>
 </html>
 
