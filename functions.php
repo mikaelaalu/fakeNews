4b8f7d292e0657354f5e2549662cb719bc659987
mikaelaalu <54809602+mikaelaalu@users.noreply.github.com>
2019-10-25 16:29:21 +0200

    Updated media queries

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
+
 declare(strict_types=1);
-require __DIR__. '/data.php'; 
+require __DIR__ . '/data.php';
 
 /**
  * Order by date
@@ -9,9 +10,9 @@ require __DIR__. '/data.php';
  * @param [array] $b
  * @return int
  */
-function compare(array $a, array $b): int 
+function compare(array $a, array $b): int
 {
- return strtotime($b['date']) - strtotime($a['date']);
+    return strtotime($b['date']) - strtotime($a['date']);
 }
 
 /**
@@ -21,11 +22,11 @@ function compare(array $a, array $b): int
  * @param [array] $authors
  * @return string
  */
-function getAuthorName (int $authorName, array $authors) : string
+function getAuthorName(int $authorName, array $authors): string
 {
     foreach ($authors as $author) {
-        if ($author['id']=== $authorName) {
-            $fullName= $author['name'];
+        if ($author['id'] === $authorName) {
+            $fullName = $author['name'];
         }
     }
     return $fullName;
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 
-require __DIR__. '/functions.php';
-require __DIR__. '/data.php';
-usort ($posts, 'compare');
+require __DIR__ . '/functions.php';
+require __DIR__ . '/data.php';
+usort($posts, 'compare');
 
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
@@ -11,37 +11,39 @@ usort ($posts, 'compare');
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="style.css">
-        <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
-        <title>Fake News</title>
-    </head>
-    <body>
-<header> 
-   <h1 class="title">Fake News </h1>
-   <p class="currentDate"><?php echo date("Y/m/d") ?> </p>
 
-</header>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <title>Fake News</title>
+</head>
 
-<div class="gridContainer">
+<body>
+    <header>
+        <h1 class="title">Fake News </h1>
+        <p class="currentDate"><?php echo date("Y/m/d") ?> </p>
 
-    <?php foreach ($posts as $post) : ?>
-    <article>
-    <h2> <?php echo $post ['title']; ?> </h2>
-    <p class="author"> <?php echo getAuthorName($post['author_id'], $authors) ?></p>
-    <img src="<?php echo $post['image'];?>"; alt="<?php echo $post['title']; ?>“>"; loading="lazy";">
-    <p class="content"> <?php echo $post ['content'] ?> </p>
-    <ul> <li class="postDate"> <?php echo "Posted " . $post ['date']  ?> </li> 
-    <li class="heart"> <img class="heartForLikes" src="likes.png"  loading=“lazy” alt=“heart“> <?php echo $post ['likes'] ?> </li> </ul>  
-</article>
-    <?php endforeach;?>
+    </header>
 
-</div>
+    <div class="gridContainer">
+
+        <?php foreach ($posts as $post) : ?>
+            <article>
+                <h2> <?php echo $post['title']; ?> </h2>
+                <p class="author"> <?php echo getAuthorName($post['author_id'], $authors) ?></p>
+                <img src="<?php echo $post['image']; ?>" ; alt="<?php echo $post['title']; ?>“>" ; loading="lazy" ;">
+                <p class="content"> <?php echo $post['content'] ?> </p>
+                <ul>
+                    <li class="postDate"> <?php echo "Posted " . $post['date']  ?> </li>
+                    <li class="heart"> <img class="heartForLikes" src="likes.png" loading=“lazy” alt=“heart“> <?php echo $post['likes'] ?> </li>
+                </ul>
+            </article>
+        <?php endforeach; ?>
+
+    </div>
 
 
 
-<?php require __DIR__. '/footer.php'; ?>
-
-
+    <?php require __DIR__ . '/footer.php'; ?>
