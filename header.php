5d13678075a680175e34f35ef097a8a6591c0931
mikaelaalu <54809602+mikaelaalu@users.noreply.github.com>
2019-10-28 11:35:23 +0100

    Added header.php

---
+++ header.php
@@ -0,0 +1,22 @@
+<?php
+require __DIR__ . '/functions.php';
+require __DIR__ . '/data.php';
+usort($posts, 'compareDate');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <title>Fake News</title>
+</head>
+
+<body>
+<header>
+        <h1 class="title">Fake News </h1>
+        <p class="currentDate"><?php echo date("Y/m/d") ?> </p>
+    </header>
